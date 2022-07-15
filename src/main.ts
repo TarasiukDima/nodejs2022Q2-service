6091b27a6f2a67e474d8a095b2cb4bc8a7189ec1
TarasiukDima <dimatarasiuk91@gmail.com>
2022-07-15 23:12:28 +0300

    feat: add work swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const docFile = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(docFile);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 };
 
